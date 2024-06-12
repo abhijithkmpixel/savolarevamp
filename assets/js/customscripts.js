d8ed5a57bd898c5ce23bdfb34f68f4cf950481fc
abhijith <abhijith@pixelflames.com>
2024-06-12 17:59:30 +0530

    asd

---
+++ assets/js/customscripts.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $('#header__search__toggle').click(function () {
+        $('.header__search ').addClass('header__search--active')
+    })
+    $(document).click(function () {
+        var $trigger = document.querySelector(
+            ".header__search "
+        );
+        if (
+            $trigger == event.target
+            || $trigger.contains(event.target)
+        ) {
+        } else {
+            $('.header__search').removeClass('header__search--active')
+        }
+    });
+    $('.hero__banner__homepage__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+    setTimeout(() => {
+        $('.hero__banner__homepage__slider .slick-dots li.slick-active').append(`    <div class="circle-container">
+        <svg>
+            <circle class="circle" cx="12" cy="12" r="10"></circle>
+        </svg>
+    </div>`)
+    }, 500);
+
+    $('.hero__banner__homepage__slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+        $('.hero__banner__homepage__slider .slick-dots li.slick-active').append(`    <div class="circle-container">
+        <svg>
+            <circle class="circle" cx="12" cy="12" r="10"></circle>
+        </svg>
+    </div>`)
+    });
+    $('.hero__banner__homepage__slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        $('.hero__banner__homepage__slider .slick-dots li.slick-active .circle-container').remove()
+    });
+    var video = $('#myVideo')[0];
+    var playButton = $('#playButton');
+    var videoOverlay = $('#overlay__blade');
+
+    playButton.on('click', function () {
+        video.play();
+        playButton.addClass('hidden');
+        videoOverlay.addClass('hidden');
+    });
+
+    $('#myVideo').on('click', function () {
+        if (video.paused) {
+            video.play();
+            playButton.addClass('hidden');
+            videoOverlay.addClass('hidden');
+        } else {
+            video.pause();
+            playButton.removeClass('hidden');
+            videoOverlay.removeClass('hidden');
+        }
+    });
+})
